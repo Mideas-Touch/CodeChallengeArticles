67049ee7e1c8c1f60552b3281ff5d15f3d08e4ae
fliblip <wickynjy@gmail.com>
2022-11-01 14:06:59 +0300

    added magazine: all_authors, all-articles

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -28,6 +28,15 @@ class Magazine
   #   Authors.all.select {|author| author.name == @name}
   # end
 
+  def all_articles
+    Article.all.filter { |article| article.magazine.name == @name}
+  end
+
+  def all_authors
+    all_articles.map { |article| 
+    Author.find_by_name(article.author)}
+  end
+
   def contributors
     Authors.all.find { |author| author == @uthor}
   end
